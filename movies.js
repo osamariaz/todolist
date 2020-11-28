bece6197f7c507cda47ae5c2da870fc3fa09e250
Muhammad Osama Riaz <osama.riaz2273@gmail.com>
2020-11-28 22:10:45 +0500

    Initialize project using Create React App

---
+++ movies.js
@@ -0,0 +1,63 @@
+/*
+	Array of genres. Iterate over this array and display buttons
+	on Web page by using each element of the array.
+*/
+
+const genres = ["Action", "Horror", "Sci-fi", "Drama", "Adventure"];
+
+
+
+
+
+
+/*
+	Array of Movies. Iterate over this array and display movie cards 
+	on web page by using each element of the array.
+*/
+const moviesList = [
+  {
+    title: "Avengers Endgame",
+    year: 2019,
+    genre: "Action",
+    cast: [
+      { name: "Robert Downey Jr", role: "Iron Man" },
+      { name: "Chris Hemsworth", role: "Thor" },
+      { name: "Mark Ruffalo", role: "Hulk" },
+    ],
+  },
+  {
+    title: "Inception",
+    year: 2010,
+    genre: "Sci-fi",
+  },
+  {
+    title: "The Imitation Game",
+    year: 2014,
+    genre: "Drama",
+  },
+  {
+    title: "The Conjuring",
+    year: 2013,
+    genre: "Horror",
+  },
+  {
+    title: "John Wick",
+    year: 2014,
+    genre: "Action",
+  },
+  {
+    title: "Spider Man Homecoming",
+    year: 2017,
+    genre: "Action",
+  },
+  {
+    title: "Inception",
+    year: 2010,
+    genre: "Sci-fi",
+  },
+  {
+    title: "The Matrix",
+    genre: "Sci-fi",
+    year: 1999,
+  },
+];
