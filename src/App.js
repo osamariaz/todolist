9e0a1118251868a471dd1e116b59b25e1c999182
Muhammad Osama Riaz <osama.riaz2273@gmail.com>
2020-11-29 13:31:38 +0500

    ka

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React ,{useState} from "react";
 import './App.css';
 
+
+
+
 function App() {
-  return (
+  var array=[];
+  const [newItem,setnewItem]=useState("");
+  const [list,setlist]=useState([]);
+  
+  //  state={
+  //         newItem:"",
+  //         list:[]
+  // }
+
+
+const updateInput=(e)=>{
+setnewItem(e.target.value);
+console.log(newItem);
+};
+
+const addItem=()=>{
+ 
+  array.push(newItem);
+  //setlist(array);
+  // this.setState({
+  //     list:array,
+  //     newItem :""
+  // });
+  
+};
+
+ 
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <h1>To Do List{array.length} </h1>
+    <form>
+        <input 
+          type='text'
+          className='input'
+          value={newItem}
+          onChange={(e) => (setnewItem(e.target.value))}
+      
+        />
+        <button className="button" onClick={addItem}>Add</button>
+      </form>
+      <ul>
+      {array.forEach(item=>{
+        <li>{item}</li>
+      })}
+       </ul>
     </div>
-  );
+)
+
 }
+
+
 
 export default App;
